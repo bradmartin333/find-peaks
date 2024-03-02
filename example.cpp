d16524a67326701035280eddf177f8ba308c20df
cob <bradmartin333@gmail.com>
2024-03-02 12:15:18 -0500

    working vscode

--- example.cpp
+++ example.cpp
@@ -3,26 +3,25 @@
 
 int main()
 {
-	//float inArr[14] = {0,1,1,1,1,1,1,5,1,1,1,1,7};
-	float inArr[4] =  { 1, 0, 0, 1 };
+	float inArr[14] = {0, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 7};
 
 	std::vector<float> in(inArr, inArr + sizeof(inArr) / sizeof(float));
 	std::vector<int> out;
 
-    PeakFinder::findPeaks(in, out, false);
+	PeakFinder::findPeaks(in, out, false);
 
-	if(out.size()==0)
+	if (out.size() == 0)
 	{
-		std::cout<<"No peaks"<<std::endl;
+		std::cout << "No peaks" << std::endl;
 		return 0;
 	}
 
-	std::cout<<"Maxima found:"<<std::endl;
+	std::cout << "Maxima found:" << std::endl;
 
-	for(int i=0; i<out.size(); ++i)
-		std::cout<<in[out[i]]<<" ";
+	for (size_t i = 0; i < out.size(); ++i)
+		std::cout << in[out[i]] << " ";
 
-	std::cout<<std::endl;
+	std::cout << std::endl;
 
 	return 0;
 }
